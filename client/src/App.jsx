b74096199570fa89f6827f2b8e15e08ae634fcfa
omkeshari245 <omkeshari245@gmail.com>
2026-08-10 13:42:58 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://three-interviewiq-gaw6.onrender.com"
+export const ServerUrl  = "https://one-interviewiq-1sqe.onrender.com"
 
 function App() {
 
